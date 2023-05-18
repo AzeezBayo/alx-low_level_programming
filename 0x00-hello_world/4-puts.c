adc001480007b2d6784337246b26eb211c46c8af
AzeezBayo <azeez.bayo@outlook.com>
2023-05-18 05:03:22 -0700

    C-hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #!/bin/bash
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 #include <stdio.h> char main(0){puts("Programming is like building a multilingual puzzle"\n");	return (0); }
