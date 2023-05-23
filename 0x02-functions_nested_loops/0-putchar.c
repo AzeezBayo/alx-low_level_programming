557dc96969a6e321f897b11977bad9caccaf5ce3
AzeezBayo <azeez.bayo@outlook.com>
2023-05-23 08:33:51 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - function that prints _putchar
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+_putchar('r');
+_putchar('p');
+_putchar('a');
+_putchar('c');
+_putchar('h');
+_putchar('u');
+_putchar('p');
+_putchar('t');
+_putchar('\n');
+
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a functions nested loops markdown
